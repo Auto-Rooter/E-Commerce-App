4bd41744805a761688406977260db6e2f4903584
Autorooter <hadi.assalem@gmail.com>
2020-08-30 14:25:59 +0200

    Add Homepage SASS File

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import './homepage.styles.scss';
 
 const HomePage = () => (
+
     <div className="homepage">
         <div className="directory-menu">
             <div className="menu-item">
